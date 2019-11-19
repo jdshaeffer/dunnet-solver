a950bff00051d68ced84f54785f31cc59cc2341a
jdshaeffer <shaefferjd@gmail.com>
2019-11-19 15:33:35 -0700

    using subprocess

--- solve.py
+++ solve.py
@@ -1,3 +1,18 @@
-from pwntools import *
+import subprocess
+from time import sleep
+
+def out(output):
+    print(output.decode('utf-8'))
+
+def write(p, command):
+    p.stdin.write(command.encode('utf-8'))
+    sleep(1)
+
+dunnet = 'emacs -batch -l dunnet'
+
+p = subprocess.Popen(dunnet.split(), stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+write(p, 'help')
+output, error = p.communicate()
+out(output)
 
 
