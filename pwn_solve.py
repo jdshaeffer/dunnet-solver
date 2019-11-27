9a5bf8176acd8f6cc440f81a1886c0de8efa63b2
jdshaeffer <shaefferjd@gmail.com>
2019-11-27 15:40:05 -0700

    moving to linux

--- pwn_solve.py
+++ pwn_solve.py
@@ -1,2 +1,6 @@
-import pwn
+from pwn import *
 
+# context.update(arch='x86_64', bits='64')
+p = process('start_dunnet')
+
+# out = p.recv().decode('utf-8').split(': ')
---
+++ start_dunnet.c
@@ -0,0 +1,3 @@
+int main() {
+    system("emacs -batch -l dunnet");
+}
