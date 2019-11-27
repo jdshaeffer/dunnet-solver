51d822551f43dfd9a781ae4f04679117181626a1
jdshaeffer <shaefferjd@gmail.com>
2019-11-27 16:59:56 -0700

    pwntools proves effective

--- pwn_solve.py
+++ pwn_solve.py
@@ -1,6 +1,31 @@
 from pwn import *
+from time import sleep
 
-# context.update(arch='x86_64', bits='64')
 p = process('start_dunnet')
 
-# out = p.recv().decode('utf-8').split(': ')
+def dis(num_lines):
+    for _ in range(num_lines):
+        print(p.readline().decode('utf-8'), end='')
+    print()
+    sleep(1)
+
+def write_slow(command):
+    for char in command:
+        print(char, end='')
+        sleep(.1)
+    print()
+
+def please(command):
+    # print(command)
+    write_slow(command)
+    p.sendline(command)
+    
+if __name__ == "__main__":
+    dis(7)
+    please('take all')
+    dis(1)
+    please('e')
+    dis(4)
+    please('e')
+    dis(3)
+
